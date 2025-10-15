de5d9a4d6408aca7f0da83fd4ab1b8edb8448dde
PolTwice <paul.alejandria@gmail.com>
2025-10-15 10:14:27 -0600

    made story manager hold script instead of each box. Connected signals to each box's buttons

--- GenericScenes/story_manager.gd
+++ GenericScenes/story_manager.gd
@@ -3,5 +3,24 @@ extends Node
 @onready var text_box: Control = $TextBox
 @onready var quiz_box: Control = $QuizBox
 
+@export var storyResource: Resource
+@export var quizResource: Resource
+
+
+enum ContentType { QUIZ, STORY }
+@export var order: Array[ContentType] = []
+
 # Story Structure?
 # hide quiz box if story,
+
+func _on_text_box_story_next_signal() -> void:
+	pass # Replace with function body.
+
+func _on_text_box_story_back_signal() -> void:
+	pass # Replace with function body.
+
+func _on_quiz_box_quiz_next_signal() -> void:
+	pass # Replace with function body.
+
+func _on_quiz_box_quiz_back_signal() -> void:
+	pass # Replace with function body.
