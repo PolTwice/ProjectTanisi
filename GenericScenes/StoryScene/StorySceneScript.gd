910ca89ff982233e6fd2ab15d4d364a31c0ac762
PolTwice <paul.alejandria@gmail.com>
2026-01-06 14:46:29 -0600

    Created new story scene for lesson restructure

---
+++ GenericScenes/StoryScene/StorySceneScript.gd
@@ -0,0 +1,5 @@
+extends CanvasLayer
+
+@onready var back: Button = $TextBox/MarginContainer/BoxDesign/TextboxMargin/VBoxContainer/ButtonsContainer/BackMarginContainer/Back
+@onready var listen_again: Button = $TextBox/MarginContainer/BoxDesign/TextboxMargin/VBoxContainer/ButtonsContainer/ListenAgainMarginContainer/ListenAgain
+@onready var continueButton: Button = $TextBox/MarginContainer/BoxDesign/TextboxMargin/VBoxContainer/ButtonsContainer/ContinueMarginContainer/Continue
