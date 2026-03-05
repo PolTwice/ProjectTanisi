7c44d069cb662546dd1b4454b28159adf315879d
PolTwice <paul.alejandria@gmail.com>
2026-03-05 11:53:23 -0600

    Adding info bars to scenes
    To help make it clear what the user should do at that time.

--- TemplateScenes/WordScene/WordSceneScript.gd
+++ TemplateScenes/WordScene/WordSceneScript.gd
@@ -1,13 +1,13 @@
 extends CanvasLayer
 
 @onready var animated_sprite_2d_node: AnimatedSprite2D = $AnimatedSprite2D
-@onready var english_reading_node: AudioStreamPlayer = $HBoxContainer/EnglishVBox/EnglishMargin/EnglishWord/EnglishReading
-@onready var cree_reading_node: AudioStreamPlayer = $HBoxContainer/CreeVBox/CreeMargin/CreeWord/CreeReading
-@onready var english_word_node: Button = $HBoxContainer/EnglishVBox/EnglishMargin/EnglishWord
-@onready var cree_word_node: Button = $HBoxContainer/CreeVBox/CreeMargin/CreeWord
+@onready var english_reading: AudioStreamPlayer = $VBoxContainer/HBoxContainer/EnglishVBox/EnglishMargin/EnglishWord/EnglishReading
+@onready var english_word: Button = $VBoxContainer/HBoxContainer/EnglishVBox/EnglishMargin/EnglishWord
 @onready var background_node: TextureRect = $Background
-@onready var narration: RichTextLabel = $Narration
+@onready var narration: RichTextLabel = $VBoxContainer/Narration
 @onready var narration_audio: AudioStreamPlayer = $NarrationAudio
+@onready var cree_reading: AudioStreamPlayer = $VBoxContainer/HBoxContainer/CreeVBox/CreeMargin/CreeWord/CreeReading
+@onready var cree_word: Button = $VBoxContainer/HBoxContainer/CreeVBox/CreeMargin/CreeWord
 
 ##EXPORTS
 @export var background: Texture
@@ -23,18 +23,18 @@ var engDone: bool = false
 var creeDone: bool = false
 
 func _on_english_word_pressed() -> void:
-	english_reading_node.play()
+	english_reading.play()
 
 func _on_cree_word_pressed() -> void:
-	cree_reading_node.play()
+	cree_reading.play()
 
 
 func _on_ready() -> void:
 	narration.text = EnglishButtonText +" in Cree is " + CreeButtonText
-	english_word_node.text = EnglishButtonText
-	cree_word_node.text = CreeButtonText
-	english_reading_node.stream = englishReading
-	cree_reading_node.stream = creeReading
+	english_word.text = EnglishButtonText
+	cree_word.text = CreeButtonText
+	english_reading.stream = englishReading
+	cree_reading.stream = creeReading
 	background_node.texture = background
 	narration_audio.stream = Reading
 	
