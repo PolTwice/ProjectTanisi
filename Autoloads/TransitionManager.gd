c0f030fa515c399ee89db4b9e38d1a60c0e06fce
PolTwice <paul.alejandria@gmail.com>
2026-03-24 13:30:15 -0600

    Adjusted Transition Manager to work properly with await

--- Autoloads/TransitionManager.gd
+++ Autoloads/TransitionManager.gd
@@ -1,25 +1,27 @@
 extends Node
 
-func fade_out(node: CanvasItem, duration:= 1.0):
+func fade_out(node: CanvasItem, duration := 1.0, alpha := 0.0) -> Tween: # Added '-> Tween'
 	if node == null:
-		return
-	
-	node.modulate.a = 1.0
+		return null
 	
 	var tween := get_tree().create_tween()
 	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(node, "modulate:a", 0.0, duration)
-	
-func fade_in(node: CanvasItem, duration:= 1.0):
+	tween.tween_property(node, "modulate:a", alpha, duration)
+	tween.tween_callback(func(): node.visible = false)
+	return tween 
+
+func fade_in(node: CanvasItem, duration := 1.0, alpha := 1.0) -> Tween:
 	if node == null:
-		return
+		return null
 	
 	node.modulate.a = 0.0
+	node.visible = true # Ensure it's visible before fading in!
 	
 	var tween := get_tree().create_tween()
 	tween.set_ease(Tween.EASE_IN)
-	tween.tween_property(node, "modulate:a", 1.0, duration)
-
+	tween.tween_property(node, "modulate:a", alpha, duration)
+	return tween 
+	
 #await this function for it to work properly
 func pause_for(duration) -> void:
 	if duration == null:
