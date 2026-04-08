644ad0f0a20a7e4e428489166323bb6a133681cf
PolTwice <paul.alejandria@gmail.com>
2026-04-08 15:20:42 -0600

    changed chapter icons

--- NonLessonScenes/chapter_select.gd
+++ NonLessonScenes/chapter_select.gd
@@ -3,12 +3,10 @@ extends CanvasLayer
 @onready var chap_1_panel: PanelContainer = $"Control/VBoxContainer/Button Margin/ButtonHbox/Chap1vBox/Chap1Panel"
 @onready var chap_2_vbox: VBoxContainer = $"Control/VBoxContainer/Button Margin/ButtonHbox/Chap2Vbox"
 
-
-func _on_chap_1_panel_gui_input(event: InputEvent) -> void:
+func _on_chap_1_button_gui_input(event: InputEvent) -> void:
 	if ((event is InputEventMouseButton and event.pressed)||(event is InputEventScreenTouch and event.pressed)):
 		GlobalState.sceneManager.changeNodeOne("res://ChapterOne/AnimalSelectScenes/0ChapterOne.tscn",)
 
-
-func _on_panel_container_2_gui_input(event: InputEvent) -> void:
+func _on_chap_2_button_gui_input(event: InputEvent) -> void:
 	if ((event is InputEventMouseButton and event.pressed)||(event is InputEventScreenTouch and event.pressed)):
 		GlobalState.sceneManager.changeNodeOne("res://ChapterTwo/LessonSelectScenes/ChapterTwoLesson.tscn",)
